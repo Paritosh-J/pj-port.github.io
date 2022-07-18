d3c4d3d8553c9fe4aab33273306f71d5a001b4af
Paritosh-J <paritoshj2001@gmail.com>
2022-07-18 13:57:15 +0530

    Updates

---
+++ precache-manifest.1a057d5c7511a5aa27bad3f09bc59d2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d13709e18065f738ddd3",
+    "url": "/static/css/main.53d24913.chunk.css"
+  },
+  {
+    "revision": "d13709e18065f738ddd3",
+    "url": "/static/js/main.d13709e1.chunk.js"
+  },
+  {
+    "revision": "b253f776025041fd57f8",
+    "url": "/static/js/1.b253f776.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae150f5fab50d5640f2ee6baf2459a91",
+    "url": "/static/media/myPic.ae150f5f.jpg"
+  },
+  {
+    "revision": "285c742951f5caaede30a2a81406bf40",
+    "url": "/static/media/myResume.285c7429.jpg"
+  },
+  {
+    "revision": "b1b541fbca9defad96fecd2755fcf20f",
+    "url": "/index.html"
+  }
+];
